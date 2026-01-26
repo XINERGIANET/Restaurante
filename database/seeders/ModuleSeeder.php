75b0a8572fcefe5d7cb26cc5f19528bfeab5478a
g013r <hectorsegundo23q>
2026-01-26 18:29:19 -0500

    bug

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ModulesMenuOptionsSeeder extends Seeder
+class ModulesSeeder extends Seeder
 {
     public function run(): void
     {
