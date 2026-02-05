1c93ac79dde31e5741919868736485d6e90710d4
Happ-TREEE <angellof.damian4@gmail.com>
2026-02-05 17:15:30 -0500

    refactor: Rutas para el tema de turno al momento de mostrar

--- routes/web.php
+++ routes/web.php
@@ -298,8 +298,8 @@ Route::middleware('auth')->group(function () {
         ->parameters(['metodos-pago' => 'paymentMethod']);
 
     //Turnos
-    Route::resource('/admin/configuracion/turnos', ShiftController::class)
-        ->names('admin.shifts')
+    Route::resource('/configuracion/turnos', ShiftController::class)
+        ->names('shifts')
         ->parameters(['turnos' => 'shifts']);
 
     //Caja chica
